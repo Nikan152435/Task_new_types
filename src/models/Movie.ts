397de796a10ba63a2191e1c4a86749fa18df7876
NikaN <NikaN7602@gmail.com>
2024-06-28 00:52:07 +0200

    change Movie.ts

--- src/models/Movie.ts
+++ src/models/Movie.ts
@@ -1,30 +1,11 @@
 export default class Movie {
-    
-    public title: string;
-    public year: number;
-    public country: string;
-    public genre: string;
-    public rating: number;
-    public director: string;
-    public actors: string[];
-  
-
     constructor(
-    title: string, 
-    year: number, 
-    country: string, 
-    genre: string, 
-    rating: number, 
-    director: string, 
-    actors: string[]
-) 
-{
-        this.title = title;
-        this.year = year;
-        this.country = country;
-        this.genre = genre;
-        this.rating = rating;
-        this.director = director;
-        this.actors = actors;
+    public title: string,
+    public year: number,
+    public country: string,
+    public genre: string,
+    public rating: number,
+    public director: string,
+    public actors: string[]
+    ) {}
     }
-}
